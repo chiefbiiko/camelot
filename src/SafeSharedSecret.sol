93369b4571af75acf7e49d0a366dd9a8f1cda832
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2023-12-01 13:50:39 +0100

    refactor: use oz _msgSender()

--- src/SafeSharedSecret.sol
+++ src/SafeSharedSecret.sol
@@ -6,28 +6,27 @@ import { OwnerManager as SafeOwnerManager } from "safe-contracts/base/OwnerManag
 
 contract SafeSharedSecret is Ownable {
 
-    modifier onlySafeSigners (address _msgSender) {
-        address[] memory _signers = SafeOwnerManager(_msgSender).getOwners();
+    modifier onlySafeSigners () {
+        address[] memory _signers = SafeOwnerManager(_msgSender()).getOwners();
         bool _isSigner = false;
         for (uint256 _i = 0; _i < _signers.length - 1; _i++) {
-            if (_msgSender == _signers[_i]) {
+            if (_msgSender() == _signers[_i]) {
                 _isSigner = true;
                 break;
             }
         }
-        require(_isSigner,"only safe signers");
+        require(_isSigner, "only safe signers");
         _;
     }
 
     /**
      * Constructs an accessoir that enables deriving a shared secret among all signers of a safe.
      * Must be deployed through a safe.
-     * @param _signers Safe signers
      */
-    //TODO read signers form safe
-    constructor( address[] memory _signers) Ownable(msg.sender) {
-        // safe = _safe;
-
+    constructor() Ownable(_msgSender()) {
+            address[] memory _signers = SafeOwnerManager(_msgSender()).getOwners();
+            // TODO setup signer queues
+            
     }
 
     /**
@@ -43,14 +42,14 @@ contract SafeSharedSecret is Ownable {
      * @param _previous Number of predecessors
      * @param _share New key share
      */
-    function submit(uint8 _previous, uint256 _share) external onlySafeSigners(msg.sender) {
+    function submit(uint8 _previous, uint256 _share) external onlySafeSigners {
         //TODO
     }
 
     /**
      * @return _shareAndPrevious (uint256 _share,uint8 _previous)
      */
-    function next() external onlySafeSigners(msg.sender) returns (uint256, uint8) {
+    function next() external onlySafeSigners returns (uint256, uint8) {
         //TODO
     }
 }
