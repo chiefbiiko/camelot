9b7af735ab10c9659339b7bab80fb5740cd153a4
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2023-12-03 10:19:26 +0100

    feat: impl done

--- src/SafeSharedSecret.sol
+++ src/SafeSharedSecret.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 import { Ownable } from "openzeppelin-contracts/access/Ownable.sol";
 import { OwnerManager as SafeOwnerManager } from "safe-contracts/base/OwnerManager.sol";
 
 contract SafeSharedSecret is Ownable {
+    enum Round { End, Ok, Idle }
 
     address public immutable safe;
     address[] public signers;
-    /// @dev Maps from signer slot to their queue.
-    mapping(uint256 => uint256[]) public queues;
+    mapping(uint256 => uint256[]) public queues; // slot=>shares
 
     modifier onlySafeSigners() {
         address[] memory _signers = SafeOwnerManager(safe).getOwners();
@@ -38,7 +38,6 @@ contract SafeSharedSecret is Ownable {
      * Safes must call this method whenever their signer set has changed.
      */
     function reconstruct() external onlyOwner {
-        // clear all queues of the old signer set
         for (uint256 _i = 0; _i < signers.length - 1; _i++) {
             delete queues[_i];
         }
@@ -51,22 +50,42 @@ contract SafeSharedSecret is Ownable {
      * @param _share New key share
      */
     function submit(uint256 _predecessors, uint256 _share) external onlySafeSigners {
-        //TODO
-        
+        uint256 _targetSlot = targetSlot(sourceSlot());
+        require(_targetSlot != type(uint256).max, "no such slot");
+        if (queues[_targetSlot].length == _predecessors){
+            queues[_targetSlot].push(_share);
+        }
     }
 
-    /** TODO TODO TODO how2 handle iterator state=> write submit first now
-     * then see how to signal step state TODO TODO
-     * 
+    /** 
      * Iterate all intermediate key shares to sign.
-     * @return _shareAndPrevious (uint256 _share,uint8 _predecessors)
+     * Round.End status 2 means there are no more prefinal rounds for given
+     * signer.
+     * @return  _status ,_share,_predecessors
      */
-    function next() external view onlySafeSigners returns (uint256, uint256) {
-        uint256 _slot = sourceSlot();
-        uint256[] storage _queue = queues[_slot];
-        uint256 _share = _queue[_queue.length - 1];
-        uint256 _predecessors = _queue.length;
-        return (_share, _predecessors);
+    function next() external view onlySafeSigners returns (Round _status, uint256 _share, uint256 _predecessors) {
+        uint256 _sourceSlot = sourceSlot();
+        uint256 _targetSlot = targetSlot(_sourceSlot);
+        require(_targetSlot != type(uint256).max, "no such slot");
+        if (queues[_targetSlot].length == queues[_sourceSlot].length) {
+            uint256[] storage _source = queues[_sourceSlot];
+            return (Round.Ok, _source[_source.length - 1], _source.length);
+        } else if (queues[_targetSlot].length == signers.length - 1) {
+            return (Round.End, 0, 0);
+        } else {
+            return (Round.Idle, 0, 0);
+        }
+    }
+
+    /**
+     * Gets the seminfinal key share of a signer ready for the 
+     * last modular exponentiation.
+     */
+    function semifinal() public view returns (uint256 _share) {
+        uint256 _sourceSlot = sourceSlot();
+        uint256[] storage _source = queues[_sourceSlot];
+        require(_source.length == signers.length - 1, "semifinal share not yet available");
+        return _source[_source.length - 1];
     }
 
     /**
@@ -75,11 +94,9 @@ contract SafeSharedSecret is Ownable {
      * part of the stored signer set.
      * @return _slot Signer slot
      */
-    function sourceSlot() public view returns (uint256) {
+    function sourceSlot() public view returns (uint256 _slot) {
         for (uint256 _i = 0; _i < signers.length - 1; _i++) {
             if (_msgSender() == signers[_i]) {
-                // _isSigner = true;
-                // break;
                 return _i;
             }
         }
@@ -88,13 +105,17 @@ contract SafeSharedSecret is Ownable {
 
     /**
      * Get the next signer's slot doing round-robin.
+     * A type(uint256).max return value indicates that _sourceSlot is not 
+     * among the stored signer set.
      * @return _slot Neighbor slot
      */
-    function targetSlot(uint256 _sourceSlot) public view returns (uint256) {
-            if (_sourceSlot == signers.length - 1) {
-                return 0;
-            } else {
-                return _sourceSlot + 1;
-            }
+    function targetSlot(uint256 _sourceSlot) public view returns (uint256 _slot) {
+        if (_sourceSlot == type(uint256).max || _sourceSlot >= signers.length) {
+            return type(uint256).max;
+        } else if (_sourceSlot == signers.length - 1) {
+            return 0;
+        } else {
+            return _sourceSlot + 1;
+        }
     }
 }
