c69979e6eebfee6fd8cfbe36821f3e6340973b75
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2023-12-05 11:47:49 +0100

    cleanup: dead code

--- test/Camelot.t.js
+++ test/Camelot.t.js
@@ -3,24 +3,11 @@ const { ethers } = require("hardhat");
 const {
   loadFixture,
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-// const {
-//   SafeFactory,
-//   Web3Adapter,
-// } = require("@safe-global/protocol-kit");
-// const chainId = require("../hardhat.config.js").networks.hardhat.chainId;
-
-// function getCreateCall() {}
-// function getFactory() {}
-// function getMultiSend() {}
-// chai.use(require("ethereum-waffle").solidity)
-// const { expect }  = chai
 
 describe("Camelot contract", function () {
   async function deploy(contractName, ...args) {
     const factory = await ethers.getContractFactory(contractName);
     const instance = await factory.deploy(...args);
-    // return instance//.deployed()
-    // console.log(">>>>>>>instance.interface",instance.interface)
     return { contract: instance, abi: instance.interface };
   }
 
@@ -38,23 +25,6 @@ describe("Camelot contract", function () {
     const camelotCode = await ethers
       .getContractFactory("Camelot")
       .then((c) => c.bytecode);
-    // console.log("singleton adddr", singleton.address)
-    // const safeProxyFactory = await deploy('_SafeProxyFactory')
-    // const multiSend = await deploy('_MultiSend')
-    // const createCall = await deploy('_CreateCall')
-    // console.log(">>>>> safeProxyFactory addr", await safeProxyFactory.contract.getAddress())
-    // console.log(">>>>> safeProxyFactory abi",  safeProxyFactory.abi)
-    // console.log(">>>>> multiSend abi",  multiSend.abi)
-    // console.log(">>>>> createCall abi",  createCall.abi)
-
-    //TODO eploy 2/3 safe like safe-tools
-    // proxyFactory.createProxyWithNonce(
-    //     address(singleton),
-    //     initData,
-    //     advancedParams.saltNonce == 0
-    //         ? uint256(keccak256(abi.encode("SAFE_TEST_TOOLS", instances.length)))
-    //         : advancedParams.saltNonce
-    // )
 
     const salt = ethers.keccak256(Buffer.from("CAMELOT", "utf8"));
 
@@ -63,31 +33,6 @@ describe("Camelot contract", function () {
       salt,
       ethers.keccak256(camelotCode)
     );
-
-    /*
-    // https://ethereum.stackexchange.com/a/116791
-    const contractNetworks = {
-      [String(chainId)]: {
-        // multiSendAddress: "<MULTI_SEND_ADDRESS>",
-        // safeMasterCopyAddress: "<MASTER_COPY_ADDRESS>",
-        // safeProxyFactoryAddress: "<PROXY_FACTORY_ADDRESS>",
-
-        safeProxyFactoryAddress: await safeProxyFactory.contract.getAddress(),
-        safeProxyFactoryAbi: safeProxyFactory.abi,
-        // multiSendAddress: await multiSend.contract.getAddress(),
-        // multiSendAbi: multiSend.abi,
-        // createCallAddress: await createCall.contract.getAddress(),
-        // createCallAbi: createCall.abi,
-      },
-    };
-    console.log(">>>>> contractNetworks chainId", chainId)
-    const ethAdapter = new Web3Adapter({ web3, signerAddress: alice })
-    console.log(">>>>> ethAdapter chainId", await ethAdapter.getChainId())
-    const safeFactory = await SafeFactory.create({
-      ethAdapter,
-      contractNetworks,
-    });
-    */
 
     return {
       alice,
@@ -105,7 +50,7 @@ describe("Camelot contract", function () {
     };
   }
 
-  //TODO test deploy camelot at fixed address
+  //WIP
   it("should deploy camelot at fixed address", async function () {
     const {
       alice,
@@ -117,9 +62,6 @@ describe("Camelot contract", function () {
       salt,
       create2CamelotAddress,
     } = await loadFixture(CamelotFixture);
-    // console.log(">>>>> createCall.address",  await createCall.contract.getAddress())
-
-    //call reatecall eprfmoreCreate2 camelotCode
     let deployedCamelotCode = await ethers.provider
       .getCode(create2CamelotAddress)
       .then((c) => c.replace("0x", ""));
