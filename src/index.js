e44af71b0c7936ff03de3ca3b20aa4cdc46fd2f3
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-01-31 16:28:59 +0100

    cleanup: leftoversx

--- src/index.js
+++ src/index.js
@@ -27,25 +27,23 @@ function scalarMult(a, b) {
   return Buffer.from(_scalarMult(a, b))
 }
 
-async function ceremony(mpx25519Address) {
-  const MPX25519 = await ethers.getContractFactory('SafeMPECDH')
-  const mpx255193 = MPX25519.attach(mpx25519Address)
+async function ceremony(mpecdhAddress) {
+  const MPECDH = await ethers.getContractFactory('SafeMPECDH')
+  const mpecdh = MPECDH.attach(mpecdhAddress)
   return {
     async step0(signer) {
       const kp = await kdf(signer)
-      await mpx255193.connect(signer).step(kp.publicKey)
-      // await mpx255193.connect(signer).done()
+      await mpecdh.connect(signer).step(kp.publicKey)
     },
     async stepN(signer) {
-      const [status, preKey] = await mpx255193.prep(signer.address)
+      const [status, preKey] = await mpecdh.prep(signer.address)
       if (status !== 1n) throw Error('expected status 1 got ' + status)
       const kp = await kdf(signer)
       const newKey = scalarMult(kp.secretKey, preKey)
-      await mpx255193.connect(signer).step(newKey)
-      // await mpx255193.connect(signer).done()
+      await mpecdh.connect(signer).step(newKey)
     },
     async stepX(signer) {
-      const [status, preKey] = await mpx255193.prep(signer.address)
+      const [status, preKey] = await mpecdh.prep(signer.address)
       if (status !== 0n) throw Error('expected status 0 got ' + status)
       const kp = await kdf(signer)
       return '0x' + hex(scalarMult(kp.secretKey, preKey))
