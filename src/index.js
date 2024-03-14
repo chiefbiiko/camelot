1da60f18d02c64ceca0e264bd2a9e46a0ae0d0e8
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-14 05:37:06 +0100

    refactor: add choreo.status()

--- src/index.js
+++ src/index.js
@@ -31,20 +31,28 @@ async function ceremony(mpecdhAddress) {
   const MPECDH = await ethers.getContractFactory('SafeMPECDH')
   const mpecdh = MPECDH.attach(mpecdhAddress)
   return {
+    async status(signer) {
+      const [status] = await mpecdh.prep(signer.address)
+      return Number(status)
+    },
     async step0(signer) {
       const kp = await kdf(signer)
       await mpecdh.connect(signer).step(kp.publicKey)
     },
     async stepN(signer) {
       const [status, preKey] = await mpecdh.prep(signer.address)
-      if (status !== 1n) throw Error('expected status 1 got ' + status)
+      if (status !== 1n) {
+        throw Error('expected status 1 got ' + status)
+      }
       const kp = await kdf(signer)
       const newKey = scalarMult(kp.secretKey, preKey)
       await mpecdh.connect(signer).step(newKey)
     },
     async stepX(signer) {
       const [status, preKey] = await mpecdh.prep(signer.address)
-      if (status !== 0n) throw Error('expected status 0 got ' + status)
+      if (status !== 0n) {
+        throw Error('expected status 0 got ' + status)
+      }
       const kp = await kdf(signer)
       return '0x' + hex(scalarMult(kp.secretKey, preKey))
     }
