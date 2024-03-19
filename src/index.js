cc088a53888d6d9a5c35f6b1d2d359d4e836d2fe
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-19 10:10:53 +0100

    wip: safe.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const { keccak256 } = require('ethers')
+const ethers = require('ethers')
 const { x25519, hashToCurve } = require('@noble/curves/ed25519')
+const { default: Safe, EthersAdapter } = require("@safe-global/protocol-kit")
+const { default: SafeApiKit } = require("@safe-global/api-kit")
 
 function generateKeyPairFromSeed(seed) {
   const p = hashToCurve(seed)
@@ -34,7 +36,7 @@ async function kdf(signer) {
   const seed = await signer
     .signMessage('MPECDH_KDF_SEED')
     .then(signedMsg =>
-      Buffer.from(keccak256(signedMsg).replace('0x', ''), 'hex')
+      Buffer.from(ethers.keccak256(signedMsg).replace('0x', ''), 'hex')
     )
   return generateKeyPairFromSeed(seed)
 }
@@ -78,6 +80,36 @@ async function ceremony(mpecdhAddress) {
       return '0x' + hex(scalarMult(kp.secretKey, preKey))
     }
   }
+}
+
+async function proposeDeploySafeMPECDH(safeAddress, signer) {
+  const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: signer })
+  const safeSigner = await Safe.create({ ethAdapter, safeAddress })
+  //TOD-O
+  // const rawData = new ethers.Interface([
+  //   "function signMessage(bytes calldata _data)"
+  // ]).encodeFunctionData("signMessage", [Buffer.from(process.env.MSG, "utf8")])
+  const safeTransactionData = {
+    // to: SIGN_MSG_LIB, TODO
+    data: rawData,
+    operation: 0, // call
+    value: 0
+  }
+  const safeTx = await safeSigner.createTransaction({
+    transactions: [safeTransactionData]
+  })
+  const apiKit = new SafeApiKit({ chainId: 100 })
+  // Deterministic hash based on transaction parameters
+  const safeTxHash = await safeSigner.getTransactionHash(safeTx)
+  // Sign transaction to verify that the transaction is coming from owner 1
+  const senderSignature = await safeSigner.signHash(safeTxHash)
+  await apiKit.proposeTransaction({
+    safeAddress,
+    safeTransactionData: safeTx.data,
+    safeTxHash,
+    senderAddress: signer.address,
+    senderSignature: senderSignature.data
+  })
 }
 
 module.exports = {
