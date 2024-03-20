8fc57268d32afa0c21e3c5e5775b8b1d8d04d6b1
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-20 09:48:15 +0100

    feat: calc mpecdh create2 addr

--- src/index.js
+++ src/index.js
@@ -4,6 +4,53 @@ const { default: Safe, EthersAdapter } = require('@safe-global/protocol-kit')
 const { default: SafeApiKit } = require('@safe-global/api-kit')
 const { abi, bytecode, deployedBytecode } = require('./SafeMPECDH.json')
 
+const CREATE_CALL_LIB = "0x9b35Af71d77eaf8d7e40252370304687390A1A52"
+
+function calculateSafeMPECDHAddress(safeAddress) {
+  return ethers.getCreate2Address(CREATE_CALL_LIB,
+    ethers.keccak256(safeAddress), ethers.keccak256(bytecode) )
+}
+
+async function proposeDeploySafeMPECDH(signer, safeAddress) {
+  const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: signer })
+  const safeSigner = await Safe.create({ ethAdapter, safeAddress })
+  // deterministic deployment via create2 using keccak256(safe) as salt
+  const data = new Contract(
+    CREATE_CALL_LIB,
+    ['function performCreate2(uint256 value, bytes memory deploymentData, bytes32 salt) public returns (address newContract)'],
+    {
+      provider: signer.provider
+    }
+  ).interface.encodeFunctionData("performCreate2", [
+    0,            
+    bytecode,                     
+    ethers.keccak256(safeAddress)
+  ])
+  const safeTxData = {
+    to: CREATE_CALL_LIB,
+    data,
+    operation: 0, // call
+    value: 0
+  }
+  const safeTx = await safeSigner.createTransaction({
+    transactions: [safeTxData]
+  })
+  const chainId = await signer.provider.getNetwork().then(({chainId}) => chainId)
+  const apiKit = new SafeApiKit({ chainId })
+  // Deterministic hash based on transaction parameters
+  const safeTxHash = await safeSigner.getTransactionHash(safeTx)
+  // Sign transaction to verify that the transaction is coming from owner 1
+  const senderSignature = await safeSigner.signHash(safeTxHash)
+  await apiKit.proposeTransaction({
+    safeAddress,
+    safeTransactionData: safeTx.data,
+    safeTxHash,
+    senderAddress: signer.address,
+    senderSignature: senderSignature.data
+  })
+  return { safeTxHash, safeAddress, mpecdhAddress: calculateSafeMPECDHAddress(safeAddress) }
+}
+
 function generateKeyPairFromSeed(seed) {
   const p = hashToCurve(seed)
   const priv = bigint2buf(p.x, 32)
@@ -88,57 +135,12 @@ async function ceremony(mpecdhAddress, provider) {
   }
 }
 
-// For contract created by CREATE2 its address will be:
-// keccak256( 0xff ++ address(this) ++ salt ++ keccak256(init_code))[12:]
-async function calculateSafeMPECDHAddress(safeAddress) {
-  
-}
-
-async function proposeDeploySafeMPECDH(signer, safeAddress) {
-  // deterministic deployment via create2 using keccak256(safe) as salt
-  const CREATE_CALL_LIB = "0x9b35Af71d77eaf8d7e40252370304687390A1A52"
-  const data = new Contract(
-    CREATE_CALL_LIB,
-    ['function performCreate2(uint256 value, bytes memory deploymentData, bytes32 salt) public returns (address newContract)'],
-    {
-      provider: signer.provider
-    }
-  ).interface.encodeFunctionData("performCreate2", [
-    0,            
-    bytecode,                     
-    ethers.keccak256(safeAddress)
-  ])
-  const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: signer })
-  const safeSigner = await Safe.create({ ethAdapter, safeAddress })
-  const safeTxData = {
-    to: CREATE_CALL_LIB,
-    data,
-    operation: 0, // call
-    value: 0
-  }
-  const safeTx = await safeSigner.createTransaction({
-    transactions: [safeTxData]
-  })
-  const chainId = await signer.provider.getNetwork().then(({chainId}) => chainId)
-  const apiKit = new SafeApiKit({ chainId })
-  // Deterministic hash based on transaction parameters
-  const safeTxHash = await safeSigner.getTransactionHash(safeTx)
-  // Sign transaction to verify that the transaction is coming from owner 1
-  const senderSignature = await safeSigner.signHash(safeTxHash)
-  await apiKit.proposeTransaction({
-    safeAddress,
-    safeTransactionData: safeTx.data,
-    safeTxHash,
-    senderAddress: signer.address,
-    senderSignature: senderSignature.data
-  })
-}
-
 module.exports = {
   hex,
   buf,
   kdf,
   scalarMult,
   ceremony,
+  calculateSafeMPECDHAddress,
   proposeDeploySafeMPECDH
 }
