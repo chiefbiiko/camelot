7f0402480141b7740280e20e5bb310a2d8f19fd4
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-20 09:49:01 +0100

    style: prettier

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ const { default: Safe, EthersAdapter } = require('@safe-global/protocol-kit')
 const { default: SafeApiKit } = require('@safe-global/api-kit')
 const { abi, bytecode, deployedBytecode } = require('./SafeMPECDH.json')
 
-const CREATE_CALL_LIB = "0x9b35Af71d77eaf8d7e40252370304687390A1A52"
+const CREATE_CALL_LIB = '0x9b35Af71d77eaf8d7e40252370304687390A1A52'
 
 function calculateSafeMPECDHAddress(safeAddress) {
-  return ethers.getCreate2Address(CREATE_CALL_LIB,
-    ethers.keccak256(safeAddress), ethers.keccak256(bytecode) )
+  return ethers.getCreate2Address(
+    CREATE_CALL_LIB,
+    ethers.keccak256(safeAddress),
+    ethers.keccak256(bytecode)
+  )
 }
 
 async function proposeDeploySafeMPECDH(signer, safeAddress) {
@@ -17,13 +20,15 @@ async function proposeDeploySafeMPECDH(signer, safeAddress) {
   // deterministic deployment via create2 using keccak256(safe) as salt
   const data = new Contract(
     CREATE_CALL_LIB,
-    ['function performCreate2(uint256 value, bytes memory deploymentData, bytes32 salt) public returns (address newContract)'],
+    [
+      'function performCreate2(uint256 value, bytes memory deploymentData, bytes32 salt) public returns (address newContract)'
+    ],
     {
       provider: signer.provider
     }
-  ).interface.encodeFunctionData("performCreate2", [
-    0,            
-    bytecode,                     
+  ).interface.encodeFunctionData('performCreate2', [
+    0,
+    bytecode,
     ethers.keccak256(safeAddress)
   ])
   const safeTxData = {
@@ -35,7 +40,9 @@ async function proposeDeploySafeMPECDH(signer, safeAddress) {
   const safeTx = await safeSigner.createTransaction({
     transactions: [safeTxData]
   })
-  const chainId = await signer.provider.getNetwork().then(({chainId}) => chainId)
+  const chainId = await signer.provider
+    .getNetwork()
+    .then(({ chainId }) => chainId)
   const apiKit = new SafeApiKit({ chainId })
   // Deterministic hash based on transaction parameters
   const safeTxHash = await safeSigner.getTransactionHash(safeTx)
@@ -48,7 +55,11 @@ async function proposeDeploySafeMPECDH(signer, safeAddress) {
     senderAddress: signer.address,
     senderSignature: senderSignature.data
   })
-  return { safeTxHash, safeAddress, mpecdhAddress: calculateSafeMPECDHAddress(safeAddress) }
+  return {
+    safeTxHash,
+    safeAddress,
+    mpecdhAddress: calculateSafeMPECDHAddress(safeAddress)
+  }
 }
 
 function generateKeyPairFromSeed(seed) {
